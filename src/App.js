b3f51d968ec3effe8d71ef9dec3a12a2d6d4290c
test <eb2ray@gmail.com>
2020-12-12 20:46:14 +0200

    Added a Todo Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
-    <h1>Hello, World!</h1>
+    <div className="App">
+        <TodoForm/>
+    </div>
   );
 }
 
